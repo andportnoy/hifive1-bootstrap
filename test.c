baf4371ac1b94527e5f77029f0ccdf0e3be7451a
Andrey Portnoy <aportnoy@fastmail.com>
2020-08-15 11:51:10 -0700

    Get the linker script to work loading from ROM to RAM

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 struct uart volatile *const uart = (void *)UARTADDR;
 
+extern u8 _ramstart, _ramend, _raminit;
 int volatile test = 0xdeadbeef;
 int main(void) {
+	memcpy(&_ramstart, &_raminit, &_ramend-&_ramstart);
+	printword(&_ramend - &_ramstart);
+	printchar('\n');
 	printword(test);
+	printchar('\n');
 }
