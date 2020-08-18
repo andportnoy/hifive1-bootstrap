513d01c948e51e8b1caabb81dfab5478a60fee22
Andrey Portnoy <aportnoy@fastmail.com>
2020-08-17 23:15:19 -0700

    Light LED within small distance

--- ultrasonic.c
+++ ultrasonic.c
@@ -46,9 +46,24 @@
 #define CLAIMPTR    ((u32 volatile *)0x0c200004)
 #define THRESHPTR   ((u32 volatile *)0x0c200000)
 
+#define SCALE 0x100
+
+struct pwm  volatile *const pwm1 = (void *)PWM1ADDR;
 struct pwm  volatile *const pwm2 = (void *)PWM2ADDR;
 struct uart volatile *const uart = (void *)UARTADDR;
 struct gpio volatile *const gpio = (void *)GPIOADDR;
+
+void intensity(u16 value) {
+	pwm1->pwmcmp2 = value;
+}
+
+void pwm1init(void) {
+	pwm1->pwmcmp0 = SCALE;     /* length of PWM cycle */
+	pwm1->pwmenalways = 1;     /* keep running PWM cycles */
+	pwm1->pwmzerocmp = 1;      /* limit PWM cycle length to pwmcmp0 */
+	pwm1->pwmscale = 0;        /* go at full speed */
+	pwm1->pwmsticky = 0;       /* let the signal deassert */
+}
 
 void gpioinit(void) {
 	/* GPIO input */
@@ -61,20 +76,25 @@ void gpioinit(void) {
 	/* PWM output */
 	gpio->iof_en   |= TRIGGER; /* use it for PWM  */
 	gpio->iof_sel  |= TRIGGER; /* PWM */
+
+	gpio->output_en  |= BIT(BLED);
+	gpio->iof_en     |= BIT(BLED);
+	gpio->out_xor    |= BIT(BLED);
+	gpio->iof_sel    |= BIT(BLED);
 }
 
 u64 delta = 0;
 __attribute__ ((noinline)) INTERRUPT(tisr) {
-	mtimecmpwr(mtimecmprd() + 0x4000);
+	mtimecmpwr(mtimecmprd() + 0x800);
 	if (delta) {
-		printdword(delta);
+		intensity(delta>>6);
+		printdword(delta>>6);
 		printchar('\n');
 		delta = 0;
 	}
 }
 
 u64 start = 0;
-u32 count = 0;
 __attribute__ ((noinline)) INTERRUPT(eisr) {
 	u32 claim = *CLAIMPTR;
 	if (gpio->rise_ip & ECHO) {
@@ -100,19 +120,20 @@ void __attribute__ ((naked, aligned(64))) vector(void) {
 	eisr();
 }
 
-void pwminit(struct pwm volatile *pwm) {
-	pwm->pwmcfg = 0;
-	pwm->pwmcmp0 = 50000;     /* half a second if a unit is 160 cycles */
-	pwm->pwmcmp2 = 50000-5;   /* on for 5*32 = 160 cycles every 0.1 s */
-	pwm->pwmenalways = 1;     /* keep running PWM cycles */
-	pwm->pwmzerocmp = 1;      /* limit PWM cycle length to pwmcmp0 */
-	pwm->pwmscale = 5;        /* 32 cycles is a unit */
-	pwm->pwmsticky = 0;       /* let the signal deassert */
+void pwm2init(void) {
+	pwm2->pwmcfg = 0;
+	pwm2->pwmcmp0 = 1000;     /* half a second if a unit is 160 cycles */
+	pwm2->pwmcmp2 = 1000-5;   /* on for 5*32 = 160 cycles every 0.1 s */
+	pwm2->pwmenalways = 1;     /* keep running PWM cycles */
+	pwm2->pwmzerocmp = 1;      /* limit PWM cycle length to pwmcmp0 */
+	pwm2->pwmscale = 5;        /* 32 cycles is a unit */
+	pwm2->pwmsticky = 0;       /* let the signal deassert */
 }
 
 int main(void) {
 	gpioinit();
-	pwminit(pwm2);
+	pwm1init();
+	pwm2init();
 
 	/* this needs clean up */
 	*PRIORITYPTR = 7;
